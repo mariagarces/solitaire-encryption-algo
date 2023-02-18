217d80c72780396c9969fce7d6b326f9ab0cc539
Maria Garces <mafegarces@hotmail.com>
2023-02-18 12:45:36 +0100

    Add basic functionnality

---
+++ card.py
@@ -0,0 +1,20 @@
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+        #AS, 2S
+
+    def get_real_value(self):
+        if self.suit == "C":
+            return self.value
+        elif self.suit == "D":
+            return self.value + 13
+        elif self.suit == "H":
+            return self.value + 26
+        elif self.suit == "S":
+            return self.value + 39
+        else:
+            return 53
+
+    def show(self):
+        print("{} {}".format(self.value, self.suit))
