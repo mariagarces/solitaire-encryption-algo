d79f37cf5ab6ff6a374336fef6b6622f4795ec83
Maria Garces <mafegarces@hotmail.com>
2023-02-18 23:23:52 +0100

    Add cors to api and fix methods

--- backend/api.py
+++ backend/api.py
@@ -1,22 +1,34 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Body
 from main import *
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 main = Main()
 
 @app.post("/message")
-def load_message(message):
+def load_message(message: str = Body(..., embed=True)):
     main.load_message(message)
     return {"response": "Message loaded"}
 
 @app.get("/cards")
 def get_key_cards():
     cards = main.get_key_cards()
-    return {cards}
+    return cards
 
 @app.get("/message/encrypt")
 def get_encrypted_message():
     encrypt_message = main.get_encrypted_message()
-    return {encrypt_message}
+    return encrypt_message
